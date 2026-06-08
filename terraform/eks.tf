08c3820e7a8a519ec2764733ce4358b3daf59d93
yogi-baba <ybhandari_ms@live.com>
2026-06-08 23:55:46 +0530

    terraform eks setup

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -44,9 +44,9 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 
   tags = merge(local.tags, {
-    Name                              = "${var.project_name}-public-${each.key}"
+    Name                                        = "${var.project_name}-public-${each.key}"
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                   = "1"
+    "kubernetes.io/role/elb"                    = "1"
   })
 }
 
@@ -100,74 +100,6 @@ resource "aws_iam_role_policy_attachment" "eks_vpc_resource_controller_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
 }
 
-resource "aws_security_group" "cluster" {
-  name        = "${var.cluster_name}-cluster-sg"
-  description = "Security group for EKS control plane"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description     = "Allow worker nodes to reach the API server"
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.nodes.id]
-  }
-
-  ingress {
-    description = "Allow same-security-group traffic"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    self        = true
-  }
-
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(local.tags, {
-    Name = "${var.cluster_name}-cluster-sg"
-  })
-}
-
-resource "aws_security_group" "nodes" {
-  name        = "${var.cluster_name}-nodes-sg"
-  description = "Security group for EKS worker nodes"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description     = "Allow node-to-node communication"
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    self            = true
-  }
-
-  ingress {
-    description     = "Allow control plane to talk to nodes"
-    from_port       = 1025
-    to_port         = 65535
-    protocol        = "tcp"
-    security_groups = [aws_security_group.cluster.id]
-  }
-
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(local.tags, {
-    Name = "${var.cluster_name}-nodes-sg"
-  })
-}
-
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster.arn
@@ -180,7 +112,6 @@ resource "aws_eks_cluster" "main" {
 
   vpc_config {
     subnet_ids              = [for subnet in aws_subnet.public : subnet.id]
-    security_group_ids      = [aws_security_group.cluster.id]
     endpoint_public_access  = true
     endpoint_private_access = false
   }
@@ -234,27 +165,6 @@ resource "aws_iam_role_policy_attachment" "node_ssm_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_launch_template" "nodes" {
-  name_prefix = "${var.cluster_name}-node-lt-"
-
-  vpc_security_group_ids = [
-    aws_security_group.nodes.id,
-    aws_security_group.cluster.id,
-  ]
-
-  tag_specifications {
-    resource_type = "instance"
-
-    tags = merge(local.tags, {
-      Name = "${var.cluster_name}-node"
-    })
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "${var.cluster_name}-ng"
@@ -271,11 +181,6 @@ resource "aws_eks_node_group" "main" {
 
   update_config {
     max_unavailable = 1
-  }
-
-  launch_template {
-    id      = aws_launch_template.nodes.id
-    version = "$Latest"
   }
 
   depends_on = [
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -28,3 +28,69 @@ variable "scan_on_push" {
   type        = bool
   default     = true
 }
+
+variable "availability_zone_count" {
+  description = "Number of availability zones to use"
+  type        = number
+  default     = 2
+}
+
+variable "project_name" {
+  description = "Project name used in naming and tagging"
+  type        = string
+  default     = "llm-chatbot"
+}
+
+variable "tags" {
+  description = "Common tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "chatapp-eks"
+}
+
+variable "kubernetes_version" {
+  description = "EKS Kubernetes version"
+  type        = string
+  default     = "1.30"
+}
+
+variable "node_instance_type" {
+  description = "EKS worker node instance type"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "node_desired_size" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "node_max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 3
+}
+
+variable "node_min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 1
+}
