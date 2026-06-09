19c9c3327e84018f9a152078b2c0aae9c442172a
yogi-baba <ybhandari_ms@live.com>
2026-06-10 00:39:55 +0530

    kube versuion update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -68,25 +68,25 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   description = "EKS Kubernetes version"
   type        = string
-  default     = "1.30"
+  default     = "1.33"
 }
 
 variable "node_instance_type" {
   description = "EKS worker node instance type"
   type        = string
-  default     = "t3.medium"
+  default     = "t3.micro"
 }
 
 variable "node_desired_size" {
   description = "Desired number of worker nodes"
   type        = number
-  default     = 2
+  default     = 1
 }
 
 variable "node_max_size" {
   description = "Maximum number of worker nodes"
   type        = number
-  default     = 3
+  default     = 2
 }
 
 variable "node_min_size" {
